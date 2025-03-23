1bc6684a72ceba308cd0d733ddd0574e37fd7c69
mcs010 <math.cds10@gmail.com>
2025-03-23 17:17:06 -0300

    feat(custom): add custom file with functions needed for processing data

---
+++ src/custom.py
@@ -0,0 +1,21 @@
+"""
+Author: Matheus Cardoso da Silva
+"""
+
+# Functions to calculate dissolved oxygen saturation (uses air temperature)
+
+def saturated_DO_concentration(temperature, altitude):
+  """
+  First step to calculate saturated DO
+  Receives the temperature and altitude of the water body
+  Returns the concentration of DO saturation
+  """
+  return (14.62-0.3898*temperature+0.006969*pow(temperature, 2)-0.00005896*pow(temperature, 3))*pow(1-0.0000228675*altitude, 5.167)
+
+
+def DO_percentage(DO, temperature, altitude):
+  """
+  Receives DO and the concentration of DO saturation
+  Return percentage of DO saturation
+  """
+  return (100*DO)/saturated_DO_concentration(temperature, altitude)
