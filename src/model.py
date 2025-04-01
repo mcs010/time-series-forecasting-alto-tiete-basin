841731b94ee2c496bf8c02868d4862c358dfecc6
mcs010 <math.cds10@gmail.com>
2025-04-01 00:07:43 -0300

    feat(model): add predict function

--- src/model.py
+++ src/model.py
@@ -16,13 +16,62 @@ from skforecast.model_selection import TimeSeriesFold
 from skforecast.model_selection import backtesting_forecaster_multiseries
 from skforecast.model_selection import bayesian_search_forecaster_multiseries
 
-def train_model(algorithm, features_list, lags, steps, series_dict_train, exog_dict_train, seed):
+from sklearn.metrics import r2_score
+
+#%%
+def train_model(algorithm, features_list, number_of_features, lags, steps, series_dict_train, exog_dict_train, seed):
     """
     Training model
     """
+
+    features = features_list[:number_of_features]
+
     if algorithm == "LGBM":
         regressor = LGBMRegressor(random_state=seed, verbose=-1, max_depth=5)
     elif algorithm == "ExtraTrees":
         regressor = ExtraTreesRegressor(random_state=seed)
     elif algorithm == "RF":
         regressor = RandomForestRegressor(random_state=seed)
+
+    forecaster = ForecasterRecursiveMultiSeries(
+                    regressor          = regressor, 
+                    lags               = lags, 
+                    encoding           = "ordinal", 
+                    dropna_from_series = False
+                )
+    
+    forecaster.fit(series=series_dict_train, exog=exog_dict_train, suppress_warnings=True)
+
+    return forecaster
+
+#%%
+def predict(forecaster, features_list, number_of_features, lags, steps, series_dict_train, series_dict, exog_dict, seed):
+
+    cv = TimeSeriesFold(
+        steps                 = steps,
+        initial_train_size    = len(series_dict_train["EMMI02900"]),
+        #refit                 = False,
+        refit                 = True,
+        allow_incomplete_fold = True,
+        fixed_train_size      = False,
+        gap                   = 0,
+    )
+
+    metrics_levels, backtest_predictions = backtesting_forecaster_multiseries(
+        forecaster            = forecaster,
+        series                = series_dict,
+        exog                  = exog_dict,
+        cv                    = cv,
+        levels                = None,
+        #metric                = "mean_absolute_error",
+        #metric = ['mean_squared_error', 'mean_absolute_error',
+        #'mean_absolute_percentage_error', 'mean_squared_log_error',
+        #'mean_absolute_scaled_error', 'root_mean_squared_scaled_error', r2_score],
+        metric                = r2_score,
+        add_aggregated_metric = True,
+        #n_jobs                ="auto",
+        n_jobs                = -1,
+        verbose               = True,
+        show_progress         = True,
+        suppress_warnings     = True
+    )
