f60cfc94b46eb1a9f7a28cb96a5cbff6071f6a6d
mcs010 <math.cds10@gmail.com>
2025-04-01 00:08:54 -0300

    feat(report): add model results plot

--- src/run_report.py
+++ src/run_report.py
@@ -24,3 +24,21 @@ for i, s in enumerate(run_experiment.series_dict.values()):
 fig.suptitle('Series in `series_dict`', fontsize=15)
 plt.savefig("../reports/figures/")
 #plt.tight_layout()
+
+#%%
+# Plot backtesting predictions
+# ==============================================================================
+fig, axs = plt.subplots(10, 1, figsize=(18, 26), sharex=True)
+
+for i, s in enumerate(series_dict.keys()):
+    axs[i].plot(series_dict[s], label=series_dict[s].name, color=colors[i])
+    axs[i].axvline(pd.to_datetime(end_train), color='white', linestyle='--', linewidth=1)  # End train
+    try:
+        axs[i].plot(backtest_predictions[s], label='prediction', color="purple")
+    except:
+        pass
+    axs[i].legend(loc='upper right', fontsize=14)
+    axs[i].tick_params(axis='both', labelsize=8)
+
+fig.suptitle('Backtest Predictions', fontsize=15)
+plt.tight_layout()
